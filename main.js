d680f7101d14a6f11c1c89ab2cc9bb134d1907df
RainbowZap223 <79471824+RainbowZap223@users.noreply.github.com>
2021-08-19 16:41:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,55 @@
-RainbowsSong = "";
-SunsetsSong = "";
+song1 = "";
+song2 = "";
+
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
 
 function preload() {
-    RainbowsSong  = loadSound("Rainbow Dash's Awesome as I Wanna Be (Instrumental).mp3");
-    SunsetsSong  = loadSound("Sunset Shimmer's  My Past Is Not Today Instrumental.mp3");
+    song1  = loadSound("Rainbow.mp3");
+    song2  = loadSound("Sunset.mp3");
 }
 
 function play() {
-    RainbowsSong.play();
-    SunsetsSong.play();
+    song1.play();
+    song2.play();
+
+    song1.setVolume(1);
+    song1.rate(1);
+
+    song2.setVolume(1);
+    song2.rate(1);
 }
 
 function setup() {
     canvas = createCanvas(600, 450);
-    canvas.center();
+    canvas.position(200, 250);
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("poseNet is initialized! :)");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left wrist x = " + leftWristX + " Left wrist y = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right wrist x = " + rightWristX + " Right wrist y = " + rightWristY);
+    }
 }
 
 function draw() {
